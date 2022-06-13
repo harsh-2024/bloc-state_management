83f2502e45899df6be9c3ff49a94fe6e84c45b61
Harsh <tripathiharsh16@gmail.com>
2022-06-14 01:47:51 +0530

    some small comments added to the code for better understanding

--- cubit_example/bloc_testing/lib/main.dart
+++ cubit_example/bloc_testing/lib/main.dart
@@ -1,3 +1,5 @@
+/* app genrating random name from the names list */
+
 import 'package:flutter/material.dart';
 import 'dart:math' as math show Random;
 import 'package:bloc/bloc.dart';
@@ -6,21 +8,25 @@ void main() {
   runApp(MyApp());
 }
 
-const names = ["Foo", "Bar", "Baz", "Harsh", "Ansh"];
+const names = ["Foo", "Bar", "Baz"];
 
+/* 
+extension method on iterable to get random element from name list.                               */
 extension RandomElement<T> on Iterable<T> {
   getRandomElement() => elementAt(math.Random().nextInt(length));
 }
 
+/* cubits constructor */
 class NamesCubit extends Cubit<String?> {
-  NamesCubit() : super(null);
-
-  void pickRandomName() => emit(names.getRandomElement());
+  NamesCubit()
+      : super(
+            null); // here initial state is null. If we want any initial state other than null use
+  // NamesCubit(String anyState) : super(String anyState);
+  // initial state.
+  void pickName() => emit(
+      names.getRandomElement()); //emit function to return the current state.
 }
 
-/*Cubit is a lightweight state management solution. 
-It is a subset of the bloc package that does not rely on 
-events and instead uses methods to emit new states. */
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -51,8 +57,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
-    cubit.close();
     // TODO: implement dispose
+    cubit.close();
     super.dispose();
   }
 
@@ -60,29 +66,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("HomePage"),
-        ),
-        body: StreamBuilder<String?>(
-            stream: cubit.stream,
-            builder: (context, snapshot) {
-              final button = TextButton(
-                  onPressed: () => cubit.pickRandomName(),
-                  child: Text("Pick a random name"));
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return button;
-
-                case ConnectionState.active:
-                  return Column(
-                    children: [Text(snapshot.data ?? " "), button],
-                  );
-
-                case ConnectionState.done:
-                  return SizedBox();
-
-                case ConnectionState.waiting:
-                  return button;
-              }
-            }));
+      title: Text("HomePage"),
+    ));
   }
 }
